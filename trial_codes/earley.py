2e9c759c395f0d4fa061b63c4e75ba3c1c229b01
Takafumi Horiuchi <hawkletter080e@icloud.com>
2018-06-23 19:01:45 +0900

    Update before implementing predictor, scanner, and completer

--- trial_codes/earley.py
+++ trial_codes/earley.py
@@ -12,20 +12,6 @@ def push(stack, state):
 def flatten_two_dim_list(two_dim_list):
     return [item for sublist in two_dim_list for item in sublist]
 
-"""
-def flatten_nest_list(nest_list):
-    working_list = nest_list
-    try:
-        check_error = working_list[0][0]
-        tmp_list = []
-        for item in working_list:
-            tmp_list.append(item)
-        tmp_list = [item for sublist in tmp_list for item in sublist]
-        flatten_nest_list(tmp_list)
-    except TypeError:
-        flat_list = working_list
-        return flat_list
-"""
 
 class EarleyParser():
     
@@ -34,13 +20,11 @@ class EarleyParser():
         self.tokens = tokens
         self.chart = [[] for _ in range(len(tokens) + 1)]
 
+
     def parse(self):
         dummy_grammar = nltk.CFG.fromstring("""gamma -> S""")
-        # print( dummy_grammar.productions()[0].lhs() )
         dummy_state = (dummy_grammar.productions()[0], 0, 0, 0)
         self.__enqueue(dummy_state, 0)
-        
-        # print(self.grammar.productions(rhs=self.tokens[0]))
         for i in range(0, len(self.tokens) + 1):
             for state in self.chart[i]:
                 if (not self.__is_complete(state) and not self.__next_is_pos(state)):
@@ -49,51 +33,56 @@ class EarleyParser():
                     self.__scanner(state)
                 else:
                     self.__completer(state)
-
         return self.chart
 
 
     # state = (rule, dot_progress, begin_idx, dot_idx)
     # 'lenght_of_right_hand_side'と'progress_of_dot'が等しければcompleteと判断
     def __is_complete(self, state):
-        rhs_len = len(state[0].rhs())
-        if (state[1] == rhs_len):
-            return True
-        else:
-            return False
-        
+        rhs_length = len(state[0].rhs())
+        dot_progress = state[1]
+        return (rhs_length == dot_progress)
+
 
     # state = (rule, dot_progress, begin_idx, dot_idx)
-    # "NT -> POS"・"POS -> word"、次の次がに単語があれば、next_is_pos == True
+    # "NT -> NT・POS", "POS -> word"、次の次がに単語があれば、next_is_pos == True
+    # 単語は、単語を左辺にもつような生成規則の集合は空、という性質を利用して得る
     def __next_is_pos(self, state):
+        dot_idx = state[1]
         current_rule = [state[0]]
-        next_rules = __get_next_rules(current_rule)
-        # if next_rules==[]: current state leads to terminal symbols
-        if not next_rules:
+        next_rules = self.__get_next_rules(current_rule, dot_idx)
+        print("next_rules:", next_rules)
+        # current state leads to terminal symbols
+        if (next_rules == []):
+            print("symbol next to dot is a word")
             return False
-        next_next_rules = __get_next_rules(next_rules)
-        # if next_next_rules: current state leads to POS
-        if not next_next_rules:
+        next_next_rules = self.__get_next_rules(next_rules)
+        print("next_next_rules:", next_next_rules)
+        # current state leads to POS
+        if (next_next_rules == []):
+            print("symbol next to dot is a POS")
             return True
         # if current state leads to non-POS/non-word
+        print("symbol next to dot is neither a word nor a POS")
         return False
-        """
-        for each_rhs_sym in current_rule.rhs():
-            next_rules = self.grammar.productions(lhs=each_rhs_sym)
-            for next_rule in next_rules:
-                for next_rule_rhs in next_rule.rhs():
-                    if next_rule_rhs in self.tokens:
-                        print("yes-next-is-pos:", next_rule)
-                        return True
-                print("no-next-is-not-pos:", next_rule)
-                return False
-        """
 
+
+    # get list of rules as argument
     # return a flat list
-    def __get_next_rules(self, rule):
+    # by specifying the dot_idx, 
+    # a list of rules begining with [symbol right after the dot] will be returned
+    # dot_index functions properly only when len(rules)==1
+    def __get_next_rules(self, rules, dot_idx=None):
         next_rules_all = []
-        for each_rhs_sym in rule.rhs():
-            next_rules_part = self.grammar.productions(lhs=each_rhs_sym)
+        if len(rules) is 1 and dot_idx is not None:
+            rhs_list = [rules[0].rhs()[dot_idx]]
+        else:
+            rhs_list = [rhs for rule in rules for rhs in rule.rhs()]
+        for each_rhs in rhs_list:
+            next_rules_part = self.grammar.productions(lhs=each_rhs)
+            # indicate that there is a word in the right-hand-side
+            if next_rules_part == []:
+                return []
             next_rules_all.append(next_rules_part)
         next_rules_all_flat = flatten_two_dim_list(next_rules_all)
         return next_rules_all_flat
